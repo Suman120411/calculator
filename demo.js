97d03e5731d7efa2f03321a0fb3cd946886b6706
suman <suman700600@gmail.com>
2025-04-06 20:41:48 +0530

    second commit

--- demo.js
+++ demo.js
@@ -1,1 +1,14 @@
-function 
+function appendvalue(value){
+    document.getElementById('display').value+=value;
+}
+function clearDisplay(){
+    document.getElementById('display').value="";
+}
+function calculationResult(){
+    try{
+        document.getElementById('display').value=eval(document.getElementById('display').value)
+    }
+    catch{
+        alert('Invalid Expression')
+    }
+}
